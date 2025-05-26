9a1284e2dae737a6da124b3b4e970c191d2ef803
Igor Astashkin <OUT-Astashkin.I.S@sbertech.ru>
2025-05-26 10:17:06 +0500

    hw06-annotation

---
+++ hw06-annotation/src/main/java/ru/demo/DemoClassTest.java
@@ -0,0 +1,35 @@
+package ru.demo;
+
+import ru.myframework.tester.annotation.After;
+import ru.myframework.tester.annotation.Before;
+import ru.myframework.tester.annotation.Test;
+
+@SuppressWarnings("java:S106")
+public class DemoClassTest {
+
+    @Before
+    void setUp() {
+        System.out.println("start tests");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("run test1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("run test2");
+        throw new RuntimeException("test failed");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("run test3");
+    }
+
+    @After
+    void end() {
+        System.out.println("finish tests");
+    }
+}
