801deee97badfeef1b47df1235f379b2f7162a38
Igor Astashkin <OUT-Astashkin.I.S@sbertech.ru>
2025-06-11 23:48:07 +0500

    hw10-bc

--- hw10-byte-codes/src/main/java/ru/otus/Ioc.java
+++ hw10-byte-codes/src/main/java/ru/otus/Ioc.java
@@ -3,6 +3,9 @@ package ru.otus;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,17 +32,21 @@ final class Ioc {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final Object target;
+        private final Set<String> targetMethodSignatures;
 
         DemoInvocationHandler(Object target) {
             this.target = target;
+            this.targetMethodSignatures = Stream.of(target.getClass().getMethods())
+                    .filter(method -> method.isAnnotationPresent(Log.class))
+                    .map(DemoInvocationHandler::methodSignature)
+                    .collect(Collectors.toSet());
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            String signature = methodSignature(method);
 
-            Method methodImpl = target.getClass().getMethod(method.getName(), method.getParameterTypes());
-
-            if (methodImpl.isAnnotationPresent(Log.class)) {
+            if (targetMethodSignatures.contains(signature)) {
                 log.info("Executed method: {}, param: {}", method.getName(), args[0]);
             }
 
@@ -49,6 +56,11 @@ final class Ioc {
         @Override
         public String toString() {
             return "DemoInvocationHandler{" + target.getClass().getName() + "=" + target + '}';
+        }
+
+        private static String methodSignature(Method method) {
+            return method.getName() + "#"
+                    + Stream.of(method.getParameterTypes()).map(Class::getName).collect(Collectors.joining(","));
         }
     }
 }
