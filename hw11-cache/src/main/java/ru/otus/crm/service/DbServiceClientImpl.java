fb802497e81d97bac6414a747b7be3c6eb92a1af
Igor Astashkin <OUT-Astashkin.I.S@sbertech.ru>
2025-08-05 13:27:33 +0500

    hw11

---
+++ hw11-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -0,0 +1,70 @@
+package ru.otus.crm.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.cache.HwCache;
+import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.sessionmanager.TransactionManager;
+import ru.otus.crm.model.Client;
+
+public class DbServiceClientImpl implements DBServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+
+    private final DataTemplate<Client> clientDataTemplate;
+    private final TransactionManager transactionManager;
+    private final HwCache<String, Client> cache;
+
+    public DbServiceClientImpl(
+            TransactionManager transactionManager,
+            DataTemplate<Client> clientDataTemplate,
+            HwCache<String, Client> cache) {
+        this.transactionManager = transactionManager;
+        this.clientDataTemplate = clientDataTemplate;
+        this.cache = cache;
+    }
+
+    @Override
+    public Client saveClient(Client client) {
+        return transactionManager.doInTransaction(session -> {
+            var clientCloned = client.clone();
+            if (client.getId() == null) {
+                var savedClient = clientDataTemplate.insert(session, clientCloned);
+                log.info("created client: {}", clientCloned);
+                var savedClientCloned = savedClient.clone();
+                cache.put(savedClientCloned.getId().toString(), savedClientCloned);
+                return savedClientCloned;
+            }
+            var savedClient = clientDataTemplate.update(session, clientCloned);
+            log.info("updated client: {}", savedClient);
+            cache.put(savedClient.getId().toString(), savedClient);
+            return savedClient;
+        });
+    }
+
+    @Override
+    public Optional<Client> getClient(long id) {
+        Client clientFromCache = cache.get(String.valueOf(id));
+
+        if (clientFromCache != null) {
+            return Optional.of(clientFromCache);
+        }
+
+        return transactionManager.doInReadOnlyTransaction(session -> {
+            var clientOptional = clientDataTemplate.findById(session, id);
+            log.info("client: {}", clientOptional);
+            clientOptional.ifPresent(client -> cache.put(client.getId().toString(), client.clone()));
+            return clientOptional;
+        });
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return transactionManager.doInReadOnlyTransaction(session -> {
+            var clientList = clientDataTemplate.findAll(session);
+            log.info("clientList:{}", clientList);
+            return clientList;
+        });
+    }
+}
