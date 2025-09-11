94356b859013a507d3014b68060561f26e1042b0
Igor Astashkin <OUT-Astashkin.I.S@sbertech.ru>
2025-09-11 16:41:48 +0500

    hw13

--- hw13-di/src/main/java/ru/otus/App.java
+++ hw13-di/src/main/java/ru/otus/App.java
@@ -2,7 +2,6 @@ package ru.otus;
 
 import ru.otus.appcontainer.AppComponentsContainerImpl;
 import ru.otus.appcontainer.api.AppComponentsContainer;
-import ru.otus.config.AppConfig;
 import ru.otus.services.GameProcessor;
 
 /*
@@ -24,18 +23,19 @@ public class App {
 
     public static void main(String[] args) {
         // Опциональные варианты
-        // AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig1.class, AppConfig2.class);
+        //         AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig1.class,
+        // AppConfig2.class);
 
         // Тут можно использовать библиотеку Reflections (см. зависимости)
-        // AppComponentsContainer container = new AppComponentsContainerImpl("ru.otus.config");
+        AppComponentsContainer container = new AppComponentsContainerImpl("ru.otus.config");
 
         // Обязательный вариант
-        AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig.class);
+        //                AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig1.class);
 
         // Приложение должно работать в каждом из указанных ниже вариантов
-        GameProcessor gameProcessor = container.getAppComponent(GameProcessor.class);
-        // GameProcessor gameProcessor = container.getAppComponent(GameProcessorImpl.class);
-        // GameProcessor gameProcessor = container.getAppComponent("gameProcessor");
+        //        GameProcessor gameProcessor = container.getAppComponent(GameProcessor.class);
+        //                GameProcessor gameProcessor = container.getAppComponent(GameProcessorImpl.class);
+        GameProcessor gameProcessor = container.getAppComponent("gameProcessor");
 
         gameProcessor.startGame();
     }
